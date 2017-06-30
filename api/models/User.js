cb2874e876fad3e9dc3ca801881fa3dbb59d24e8
Karla Denia Salas Jiménez <Karla Denia Salas Jimnez>
2017-06-30 17:23:47 -0500

    feat: add basic CRUD

---
+++ api/models/User.js
@@ -0,0 +1,36 @@
+/**
+ * User.js
+ *
+ * @description :: Implementacion de un modelo de usuario
+ */
+const bcrypt=require('bcrypt');
+module.exports = {
+  attributes: {
+	 nombre:{
+		type:'string',
+		required: true,
+	 },
+	 email: {
+            type: 'email',
+            required: true,
+            unique: true
+        },
+        password: {
+            type: 'string',
+            required: true
+        },
+		bando:{
+		type:'string',
+		required: true,
+		},
+  },
+  beforeCreate:(values,cb) => {
+	return bcrypt.hash(values.password, 18, (err,hash)=>{
+		if(err){
+			return cb(err);
+		}
+		values.password=hash;
+		return cb(null,values);
+	});
+  },
+};
